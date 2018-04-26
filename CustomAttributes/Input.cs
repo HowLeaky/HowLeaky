c73ceb504822fb747fdecbb4accbcced41434d3b
Al Doherty <al.doherty73@gmail.com>
2018-04-26 14:00:17 +1000

    Fix Input Attribute

--- CustomAttributes/Input.cs
+++ CustomAttributes/Input.cs
@@ -8,6 +8,15 @@ namespace HowLeaky.CustomAttributes
 {
     public class Input : Attribute
     {
+        public string Display { get; set; }
+        public string Unit { get; set; }
+        public string Description { get; set; }
         //This is just a tag at the moment
+        public Input(string Display, string Unit = null, string Description = null)
+        {
+            this.Display = Display;
+            this.Unit = Unit;
+            this.Description = Description;
+        }
     }
 }
